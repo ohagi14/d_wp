355fb39557b47133beb9ba7e48aef36977fa60a7
ohagi14 <furikake.hs@gmail.com>
2025-07-14 13:50:49 +0900

    start

--- wp-content/themes/mt/functions.php
+++ wp-content/themes/mt/functions.php
@@ -1,6 +1,6 @@
 <?php
 function mt_scripts()
 {
-	wp_enqueue_style('mt-style', get_template_directory_uri() . '/assets/styles/main.css', array(), filemtime(get_template_directory() . '/assets/styles/main.css'));
+	wp_enqueue_style('mt-style', get_template_directory_uri() . '/assets/styles/main.min.css', array(), filemtime(get_template_directory() . '/assets/styles/main.min.css'));
 }
 add_action('wp_enqueue_scripts', 'mt_scripts');
