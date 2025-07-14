1d99fa3cae1554dc66d9e397d6210f1089544af6
ohagi14 <furikake.hs@gmail.com>
2025-07-14 14:00:44 +0900

    初期設定

---
+++ wp-content/themes/mt/assets/scripts/app.js
@@ -0,0 +1,11 @@
+// jQueryを引数として受け取り、関数内では $ を安全に使えるようにする
+(function ($) {
+	'use strict';
+
+	// DOMが読み込まれたら実行
+	$(function () {
+		// ここにコードを書く
+		console.log('jQuery is ready!');
+	});
+
+})(jQuery);
--- wp-content/themes/mt/functions.php
+++ wp-content/themes/mt/functions.php
@@ -1,6 +1,25 @@
 <?php
-function mt_scripts()
+function my_theme_scripts()
 {
-	wp_enqueue_style('mt-style', get_template_directory_uri() . '/assets/styles/main.min.css', array(), filemtime(get_template_directory() . '/assets/styles/main.min.css'));
+	// Google Fonts
+	wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap', array(), null);
+
+	// メインのCSS
+	wp_enqueue_style('main-style', get_template_directory_uri() . '/assets/styles/main.min.css', array(), '1.0.0');
+
+	// jQuery (WordPressに同梱されているものを利用)
+	wp_enqueue_script('jquery');
+
+	// メインのJS
+	wp_enqueue_script('main-script', get_template_directory_uri() . '/assets/scripts/app.js', array('jquery'), '1.0.0', true);
 }
-add_action('wp_enqueue_scripts', 'mt_scripts');
+// wp_enqueue_scripts アクションフックに関数を登録
+add_action('wp_enqueue_scripts', 'my_theme_scripts');
+
+
+// <title>タグをWordPressで管理する
+function my_theme_setup()
+{
+	add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'my_theme_setup');
